e822c116c96750216132f35e318d2c95740c8c4e
Himal bam <himalbam1212@gmail.com>
2023-12-28 09:57:45 +0545

    pong update

--- main.lua
+++ main.lua
@@ -10,7 +10,13 @@ VIRTUAL_HEIGHT = 243
 
 function love.load()
 
+    
+
     love.graphics.setDefaultFilter('nearest','nearest')
+
+    smallFont = love.graphics.newFont('font.ttf', 8)
+
+    love.graphics.setFont(smallFont)
     push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH,WINDOW_HEIGHT ,{
         fullscreen = false,
         resizable = false,
@@ -36,7 +42,13 @@ end
 
 function love.draw()
     push:apply('start')
+
+    love.graphics.clear(40,45,52,255)
     love.graphics.printf( "hello", 0,VIRTUAL_HEIGHT/2 - 6, VIRTUAL_WIDTH, 'center')
 
+    love.graphics.rectangle("fill", 20,50, 60,120)
+
+    love.graphics.rectangle('fill',VIRTUAL_WIDTH - 10 , VIRTUAL_HEIGHT - 50, 5 , 20)
+    love.graphics.rectangle('fill',VIRTUAL_WIDTH /2 - 2 , VIRTUAL_HEIGHT /2 - 2 , 4 , 4)
     push:apply('end')
 end
